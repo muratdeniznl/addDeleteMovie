f5cb915539cee0573db6cfdec3188b23d1c61f18
Dmitry Matsalishenko <mrtdnztr@gmail.com>
2023-07-05 14:28:26 +0200

    install project

---
+++ src/main/java/com/appiekap/movietheater/model/Movie.java
@@ -0,0 +1,49 @@
+package com.appiekap.movietheater.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public Movie() {
+    }
+
+    public Movie(long id, String title, boolean watched) {
+        this.id = id;
+        this.title = title;
+        this.watched = watched;
+    }
+
+    private String title;
+
+    private boolean watched;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isWatched() {
+        return this.watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+}
